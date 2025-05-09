99e1b240b8bc2e4d4759809e5a8cfed829a7deee
Mojujo <oscar.tornqvist01@gmail.com>
2025-05-09 12:00:03 +0200

    Fix: removed faulty test

--- src/components/CustomButton.test.tsx
+++ src/components/CustomButton.test.tsx
@@ -47,8 +47,4 @@ describe("Custom Button Component States", () => {
         fireEvent.click(customButtonComponent)
         expect(customButtonComponent).toHaveClass(ButtonState.clicked)
     })
-
-    test ("This test should fail for my Github Action Training", () => {
-        expect(2+2).toBe(999)
-    })
 })
